dd84830dabcb8feafa087c6c5c317c3efe3790d0
dingyuc <dingyuc@yonyou.com>
2020-08-24 16:57:32 +0800

    2020-08-24 16:57

--- bin/index.js
+++ bin/index.js
@@ -4,5 +4,7 @@ let child_process = require('child_process');
 child_process.exec('git clone https://github.com/williamyu185/yonyou-cli.git', {}, function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
+    }else {
+        console.log('Download successful!');
     }
 });
