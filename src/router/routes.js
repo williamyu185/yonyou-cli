c37c0fd6f5a2a5b9769db24969f398ee9bbf0b91
dingyuc <dingyuc@yonyou.com>
2020-08-24 13:46:47 +0800

    2020-08-24 13:44

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,4 @@
 import index from '../components/index.vue';
-import delay from '../components/supervise/delay.vue';
-import stop from '../components/supervise/stop.vue';
-import setting from '../components/supervise/setting.vue';
-import registerList from '../components/supervise/registerList.vue';
-import registerDetail from '../components/supervise/registerDetail.vue';
-import handle from '../components/supervise/handle.vue';
-import speedOfProgress from '../components/supervise/speedOfProgress.vue';
-import urgeRecord from '../components/supervise/urgeRecord.vue';
-import empower from '../components/supervise/empower.vue';
-import iframeContainer from '../components/supervise/iframeContainer.vue';
-import search from '../components/supervise/search.vue';
 
 export default [
     {
@@ -22,61 +11,5 @@ export default [
         name: 'index',
         path: '/index',
         component: index,
-        redirect: {
-            name: 'registerList'
-        },
-        children: [
-            {
-                name: 'registerList',
-                path: 'registerList',
-                component: registerList, 
-            }, {
-                name: 'setting',
-                path: 'setting',
-                component: setting,
-            }, {   
-                name: 'delay',
-                path: 'delay',
-                component: delay,
-            }, {
-                name: 'stop',
-                path: 'stop',
-                component: stop,
-            }, {
-                name: 'registerDetail',
-                path: 'registerDetail',
-                component: registerDetail,
-                children: [
-                    {
-                        name: 'speedOfProgress',
-                        path: 'speedOfProgress',
-                        component: speedOfProgress, 
-                    },
-                    {
-                        name: 'urgeRecord',
-                        path: 'urgeRecord',
-                        component: urgeRecord, 
-                    },
-                    {
-                        name: 'empower',
-                        path: 'empower',
-                        component: empower, 
-                    },
-                    {
-                        name: 'iframeContainer',
-                        path: 'iframeContainer',
-                        component: iframeContainer, 
-                    }
-                ]
-            }, {
-                name: 'handle',
-                path: 'handle',
-                component: handle,
-            }, {
-                name: 'search',
-                path: 'search',
-                component: search
-            }
-        ]
     }
 ];
