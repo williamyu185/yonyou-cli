693b5ee1b242d2c239291f3a4cfe17c8d9f206c6
dingyuc <dingyuc@yonyou.com>
2020-08-24 16:59:03 +0800

    2020-08-24 16:59

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 let child_process = require('child_process');
 
-child_process.exec('git clone https://github.com/williamyu185/yonyou-cli.git', {}, function (error, stdout, stderr) {
+child_process.exec('git clone https://github.com/williamyu185/yonyou-cli.git coordination-cli', {}, function (error, stdout, stderr) {
     if (error !== null) {
       console.log('exec error: ' + error);
     }else {
