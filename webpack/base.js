09cd3255ab5f006baef0a927bfc1fa2615771ce7
dingyuc <dingyuc@yonyou.com>
2020-08-25 14:41:14 +0800

    2020-08-25 14:41

--- webpack/base.js
+++ webpack/base.js
@@ -39,39 +39,40 @@ const cssLoaderMiniCssExtractPluginRule = {
 		publicPath: '../'
 	}
 };
-// const pxToViewport = require('postcss-px-to-viewport');
-// const pxToViewportConfig = {
-// 			// 要转化的单位
-// 			unitToConvert: "px",
-// 			// UI设计稿的宽度
-// 			viewportWidth: 750, 
-// 			// 转换后的精度，即小数点位数
-// 			unitPrecision: 6,
-// 			// 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
-// 			propList: ["*"],
-// 			// 指定需要转换成的视窗单位，默认vw
-// 			viewportUnit: "vw",
-// 			// 指定字体需要转换成的视窗单位，默认vw
-// 			fontViewportUnit: "vw",
-// 			// 指定不转换为视窗单位的类名，
-// 			selectorBlackList: ["nopxtoviewport"], 
-// 			// 默认值1，小于或等于1px则不进行转换
-// 			minPixelValue: 1,
-// 			// 是否在媒体查询的css代码中也进行转换，默认false
-// 			mediaQuery: false,
-// 			// 是否转换后直接更换属性值
-// 			replace: true,
-// 			// 设置忽略文件，用正则做目录名匹配
-// 			exclude: [/node_modules/],
-// 			// 是否处理横屏情况
-// 			landscape: false 
-// 		};
+const pxToViewport = require('postcss-px-to-viewport');
+const pxToViewportConfig = {
+			// 要转化的单位
+			unitToConvert: "px",
+			// UI设计稿的宽度
+			viewportWidth: 750, 
+			// 转换后的精度，即小数点位数
+			unitPrecision: 6,
+			// 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+			propList: ["*"],
+			// 指定需要转换成的视窗单位，默认vw
+			viewportUnit: "vw",
+			// 指定字体需要转换成的视窗单位，默认vw
+			fontViewportUnit: "vw",
+			// 指定不转换为视窗单位的类名，
+			selectorBlackList: ["nopxtoviewport"], 
+			// 默认值1，小于或等于1px则不进行转换
+			minPixelValue: 1,
+			// 是否在媒体查询的css代码中也进行转换，默认false
+			mediaQuery: false,
+			// 是否转换后直接更换属性值
+			replace: true,
+			// 设置忽略文件，用正则做目录名匹配
+			exclude: [/node_modules/],
+			// 是否处理横屏情况
+			landscape: false 
+		};
 const postcssLoaderRules = {
 	loader: 'postcss-loader',
 	options: {
 		plugins: [
-			//自动补全css前缀
+			// 自动补全css前缀
 			require('autoprefixer'),
+			// 如果需要开启px转vw插件功能,请取消注释此行代码
 			// pxToViewport(pxToViewportConfig)
 		],
 		// 开启sourceMap调试
