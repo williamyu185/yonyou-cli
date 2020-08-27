f5a831fbcf80f18b11271ded5021c0bce29f429b
dingyuc <dingyuc@yonyou.com>
2020-08-27 13:17:53 +0800

    2020-08-27 13:17

--- bin/index.js
+++ bin/index.js
@@ -9,12 +9,14 @@ let isShellParam = function(param) {
   return /^-/ig.test(item);
 };
 arguments.forEach((item, index) => {
+  if((index == 0) && !isShellParam(item)) {
+    shellMsg.projectName = item;
+    return;
+  }
   if(isShellParam(item)) {
     if(item == '-i' || item == '--install') {
       isInstall = true;
     }
-  }else {
-    (index == 0) && (shellMsg.projectName = item);
   }
 });
 let afterClone = 'cd ./' + shellMsg.projectName + ' && rm -rf .git && cd ../';
@@ -25,10 +27,11 @@ child_process.exec('git clone https://github.com/williamyu185/yonyou-cli.git ' +
     if (error !== null) {
       console.log('exec error: ' + error);
     }else {
-      console.log('Download successful!');
       child_process.exec(afterClone, {}, function (error, stdout, stderr) {
         if (error !== null) {
           console.log('exec error: ' + error);
+        }else {
+          console.log('Project created successfully!');
         }
       });
     }
