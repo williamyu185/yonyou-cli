48d2a6610b6a040cd2a04c2dd58d71e5e9b879d3
dingyuc <dingyuc@yonyou.com>
2020-11-19 14:43:32 +0800

    2020-11-19 14:43

--- bin/index.js
+++ bin/index.js
@@ -41,7 +41,7 @@ if(shellMsg.isCopyOneOfENVToDist) {
     return new Promise((resolve, reject) => {
       child_process.exec(`rm -rf ./${ENV_dist} ${dist} && unzip -o ./${ENV_dist}.zip -d ${ENV_dist}`, (error, stdout, stderr) => {
         if (error !== null) {
-          reject(error);
+          resolve(error);
         }else {
           resolve(null);
         }
@@ -51,7 +51,7 @@ if(shellMsg.isCopyOneOfENVToDist) {
   let compatibleSolutions = async () => {
     let error = await unzip_ENV_dist();
     if(error !== null) {
-      return Promise.reject(error);
+      return Promise.resolve(error);
     }else {
       return Promise.resolve(null);
     }
