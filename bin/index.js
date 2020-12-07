677a44481d93df5200020bdfddcb3a62019acfa8
dingyuc <dingyuc@yonyou.com>
2020-12-07 15:04:37 +0800

    2020-12-07 15:04  stdio

--- bin/index.js
+++ bin/index.js
@@ -42,7 +42,7 @@ if(shellMsg.isCopyOneOfENVToDist) {
   let unzip_ENV_dist = () => {
     return new Promise((resolve, reject) => {
       child_process.execSync(`rm -rf ./${ENV_dist} ${dist}`, {stdio: 'inherit'});
-      child_process.exec(`unzip -o ./${ENV_dist}.zip -d ./${ENV_dist}`, {stdio: 'inherit'}, (error, stdout, stderr) => {
+      child_process.exec(`unzip -o ./${ENV_dist}.zip -d ./${ENV_dist}`, {}, (error, stdout, stderr) => {
         if (error !== null) {
           resolve(error);
         }else {
@@ -67,12 +67,12 @@ if(shellMsg.isCopyOneOfENVToDist) {
       let unzip = new AdmZip(`${ENV_dist}.zip`);
       unzip.extractAllTo(`${ENV_dist}`, true);
     }
-    child_process.exec(`cp -fr ./${ENV_dist}/${copyENV}/. ${dist}`, {stdio: 'inherit'}, (error, stdout, stderr) => {
+    child_process.exec(`cp -fr ./${ENV_dist}/${copyENV}/. ${dist}`, {}, (error, stdout, stderr) => {
       if(error !== null) {
         console.log('exec error: ' + error);
       }else {
         console.log(`copy ${copyENV} successfully!`);
-        child_process.exec(`rm -rf ./${ENV_dist}`, {stdio: 'inherit'});
+        child_process.exec(`rm -rf ./${ENV_dist}`, {});
       }
     });
   });
@@ -92,7 +92,7 @@ if(shellMsg.isPublish) {
     let ENVConfig = bale[ENV];
     allPromise.push(new Promise((resolve, reject) => {
       let crossEnv = ENVConfig['NODE_ENV'] || ENV;
-      child_process.exec(ENVConfig.execShell || `cross-env NODE_ENV=${crossEnv} webpack --progress --config ./webpack/${ENVConfig.webpackFile || ENV}.js`, {stdio: 'inherit'}, (error, stdout, stderr) => {
+      child_process.exec(ENVConfig.execShell || `cross-env NODE_ENV=${crossEnv} webpack --progress --config ./webpack/${ENVConfig.webpackFile || ENV}.js`, {}, (error, stdout, stderr) => {
         if (error !== null) {
           console.log(`exec error: ${error}`);
           reject(error)
@@ -106,7 +106,7 @@ if(shellMsg.isPublish) {
   Promise.all(allPromise).then((arr) => {
     console.log(`All environments were packaged successfully!`);
     // new Promise((resolve, reject) => {
-    //   child_process.exec(`zip -r ./${ENV_dist}.zip ./${ENV_dist} && rm -rf ./${ENV_dist}`, {stdio: 'inherit'}, (error, stdout, stderr) => {
+    //   child_process.exec(`zip -r ./${ENV_dist}.zip ./${ENV_dist} && rm -rf ./${ENV_dist}`, {}, (error, stdout, stderr) => {
     //     if (error !== null) {
     //       reject(error);
     //     }else {
@@ -114,18 +114,18 @@ if(shellMsg.isPublish) {
     //     }
     //   });
     // }).then(() => {
-    //   child_process.exec(`rm -rf ./${ENV_dist}`, {stdio: 'inherit'});
+    //   child_process.exec(`rm -rf ./${ENV_dist}`, {});
     // }, () => {
     //   let zip = new AdmZip();
     //   zip.addLocalFolder(`${ENV_dist}`);
     //   zip.writeZip(`${ENV_dist}.zip`);
-    //   child_process.exec(`rm -rf ./${ENV_dist}`, {stdio: 'inherit'});
+    //   child_process.exec(`rm -rf ./${ENV_dist}`, {});
     // });
     let AdmZip = require('adm-zip');
     let zip = new AdmZip();
     zip.addLocalFolder(`${ENV_dist}`);
     zip.writeZip(`${ENV_dist}.zip`);
-    child_process.exec(`rm -rf ./${ENV_dist}`, {stdio: 'inherit'});
+    child_process.exec(`rm -rf ./${ENV_dist}`, {});
   }).catch((error) => {
     console.log('dist package failed');
   });;
@@ -140,18 +140,22 @@ if(shellMsg.isInstall) {
 }else {
   afterClone += ` && cd ../`;
 }
-console.log('如因网络原因，执行指令后项目长时间未创建，请直接下载源码zip包')
-console.log('下载地址：https://github.com/williamyu185/yonyou-cli')
-child_process.exec(`git clone https://github.com/williamyu185/yonyou-cli.git ${shellMsg.projectName}`, {stdio: 'inherit'}, (error, stdout, stderr) => {
+console.log('如因网络原因，执行指令后项目长时间未创建，请直接下载源码zip包');
+console.log('下载地址：https://github.com/williamyu185/yonyou-cli');
+let cloneProjectError = null;
+child_process.execSync(`git clone https://github.com/williamyu185/yonyou-cli.git ${shellMsg.projectName}`, {stdio: 'inherit'}, (error, stdout, stderr) => {
     if (error !== null) {
+      cloneProjectError = error;
       console.log(`exec error: ${error}`);
-    }else {
-      child_process.exec(afterClone, {stdio: 'inherit'}, (error, stdout, stderr) => {
-        if (error !== null) {
-          console.log(`exec error: ${error}`);
-        }else {
-          console.log('Project created successfully!');
-        }
-      });
     }
+});
+if(!cloneProjectError) {
+  return;
+}
+child_process.exec(afterClone, {}, (error, stdout, stderr) => {
+  if (error !== null) {
+    console.log(`exec error: ${error}`);
+  }else {
+    console.log('Project created successfully!');
+  }
 });
