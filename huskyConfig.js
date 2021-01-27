2e18939f2b382916961c49cfab5b6996c7f8e373
dingyuc <dingyuc@yonyou.com>
2021-01-27 10:47:01 +0800

    2021-01-27 10:46

--- huskyConfig.js
+++ huskyConfig.js
@@ -2,5 +2,5 @@ module.exports = {
     // 日常操作时，是禁止将测试环境分支代码合并进任何非测试环境分支的
     // 此字段为各测试环境分支名，比如测试环境分支名为test，配置字段为['test']后
     // 当在任何非测试分支（比如master）执行git pull origin test或git merge test时，都会给出醒目的提示警告
-    testBranch: ['test']
+    testBranch: []
 };
